7f70a489c4f64a7cf782c6388d6449c9ff9245c9
vogtn <vogtn@uw.edu>
2016-06-26 16:43:27 -0700

    establishing middleware for express
    custom functions that the app can make use of

--- nodetest1/app.js
+++ nodetest1/app.js
@@ -26,6 +26,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//make our db accessible to our router
+app.use(function(req,res,next){
+  req.db = db;
+  next();
+});
+
 app.use('/', routes);
 app.use('/users', users);
 
