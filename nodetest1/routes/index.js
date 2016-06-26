57b8b0feb218751f513b05633cfb50635c29df28
vogtn <vogtn@uw.edu>
2016-06-26 16:48:56 -0700

    extracting db object
    extracting the db object we passed to http request, using the connection
    to fill "docs" with database documents (the user data).
    Second, does a page render.

--- nodetest1/routes/index.js
+++ nodetest1/routes/index.js
@@ -13,3 +13,14 @@ router.get('/helloworld',function(req,res){
 });
 
 module.exports = router;
+
+/* Get the Userlist page */
+router.get('/userlist', function(req,res){
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({},{},function(e,docs){
+    res.render('userlist',{
+      "userlist": docs
+    });
+  });
+});
