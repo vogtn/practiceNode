e2c29fd7fb76ef74459efcbfeb5ce1929c8747cd
vogtn <vogtn@uw.edu>
2016-06-27 11:38:29 -0700

    creating DB function

--- nodetest1/routes/index.js
+++ nodetest1/routes/index.js
@@ -17,6 +17,36 @@ router.get('/newuser', function(req,res){
   res.render('newuser',{title:'Add New User'});
 });
 
+/* Post to Add User service */
+router.post('/adduser',function(req,res){
+
+  //set internal DB variable
+  var db = req.db;
+
+  //Get form values. These rely on the "name" attributes
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+
+  //set collection
+  var collection = db.get('usercollection');
+
+  //submit to the DB
+  colection.insert({
+    "username": userName,
+    "email": useremail
+  }, function(err,doc){
+    if(err){
+      //if it fails, return error
+      res.send("There was a problem adding the information to the database.");
+    }
+    else{
+      //and forward to sucess page
+      res.redirect("userlist");
+    }
+});
+
+});
+
 module.exports = router;
 
 
